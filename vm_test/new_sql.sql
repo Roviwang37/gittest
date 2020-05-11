9c3a7ec7720bd86701f6d56d8f4c5b9927a9de93
Roviwang37 <roviwang@gmail.com>
2020-05-11 15:56:57 -0400

    this is the initial commit

---
+++ vm_test/new_sql.sql
@@ -0,0 +1,7 @@
+SELECT start_terminal,
+       duration_seconds,
+       SUM(duration_seconds) OVER
+         (PARTITION BY start_terminal) AS start_terminal_total
+  FROM tutorial.dc_bikeshare_q1_2012
+ WHERE start_time < '2012-01-08'
+
